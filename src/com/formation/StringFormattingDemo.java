28a92a32c481961918a9cc2097c033cf4715ad14
Evgeny <evgeny.rfi@gmail.com>
2022-01-31 20:30:39 +0100

    strings formating demo added

---
+++ src/com/formation/StringFormattingDemo.java
@@ -0,0 +1,21 @@
+package com.formation;
+
+public class StringFormattingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("===== String formatting =====");
+        System.out.println();
+        String firstName = "Evgeny";
+
+        String greetingsTemplate = "Hello, %s! Have a nice %s!";
+
+        String morning = "morning";
+        String afternoon = "afternoon";
+        String evening = "evening";
+        String formattedString = String.format(greetingsTemplate, firstName, evening);
+        System.out.println(formattedString);
+
+        System.out.printf("You have %d computers available at store\n", 7);
+    }
+
+}
