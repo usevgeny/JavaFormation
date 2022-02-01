f56f02967105e820b7ffcea5243d3aebbe35aa85
Evgeny <evgeny.rfi@gmail.com>
2022-02-01 17:09:27 +0100

    regex appended with some additionnal expressions

--- src/com/formation/regexExamples.java
+++ src/com/formation/regexExamples.java
@@ -21,7 +21,22 @@ public class regexExamples {
         String[] sentences = sampleText2.split("[\\.!?]");
         System.out.println(Arrays.toString(sentences));
 
+/*
+				\p{Lower}   A lower-case alphabetic character: [a-z]
+				\p{Upper}   An upper-case alphabetic character:[A-Z]
+				\p{ASCII}   All ASCII:[\x00-\x7F]
+				\p{Alpha}   An alphabetic character:[\p{Lower}\p{Upper}]
+				\p{Digit}   A decimal digit: [0-9]
+				\p{Alnum}   An alphanumeric character:[\p{Alpha}\p{Digit}]
+				\p{Punct}   Punctuation: One of !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+				\p{Graph}   A visible character: [\p{Alnum}\p{Punct}]
+				\p{Print}   A printable character: [\p{Graph}\x20]
+				\p{Blank}   A space or a tab: [ \t]
+				\p{Cntrl}   A control character: [\x00-\x1F\x7F]
+				\p{XDigit}  A hexadecimal digit: [0-9a-fA-F]
+				\p{Space}   A whitespace character: [ \t\n\x0B\f\r]
 
+		*/
 
 
 
