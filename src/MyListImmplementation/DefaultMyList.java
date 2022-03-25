e7bc5382cf1ba95e998853217ca2964acb63bbb2
Evgeny <evgeny.rfi@gmail.com>
2022-03-25 13:30:28 +0100

    started hw with my list

---
+++ src/MyListImmplementation/DefaultMyList.java
@@ -0,0 +1,47 @@
+package MyListImmplementation;
+
+public class DefaultMyList implements MyList{
+
+	@Override
+	public void add(Object e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(MyList c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
